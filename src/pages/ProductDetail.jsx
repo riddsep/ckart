2502f63bb4cb95c9ea7d4c3213d9a6e08a3dd7b8
riddsep <ridoseptiawan00@gmail.com>
2025-01-28 22:35:26 +0700

    feat(component): add description product detail component

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -201,18 +201,32 @@ function ProductDetail() {
                   <div>
                     <label htmlFor="name">
                       Name
-                      <input type="text" id="name" />
+                      <input
+                        type="text"
+                        id="name"
+                        placeholder="Enter Your Name"
+                      />
                     </label>
                     <label htmlFor="email">
                       Email
-                      <input type="text" id="email" />
+                      <input
+                        type="text"
+                        id="email"
+                        placeholder="Enter Your Email"
+                      />
                     </label>
                   </div>
                   <StarRating />
                   <label htmlFor="message">
                     Message
-                    <textarea id="message" />
+                    <textarea
+                      id="message"
+                      placeholder="Write Your Message Here"
+                    />
                   </label>
+                  <Button $fullWidth $variant="primary">
+                    Submit <img src="/icons/arrow-right.svg" alt="" />
+                  </Button>
                 </form>
               </AddReviews>
             </Reviews>
@@ -445,7 +459,10 @@ const OrderInfo = styled.div`
     gap: 10px;
   }
 `;
-const Reviews = styled.div``;
+const Reviews = styled.div`
+  display: flex;
+  gap: 30px;
+`;
 
 const ReviewList = styled.div`
   display: flex;
@@ -454,16 +471,46 @@ const ReviewList = styled.div`
 `;
 
 const AddReviews = styled.div`
+  padding: 20px;
+
+  h3 {
+    font-size: 24px;
+    margin-block-end: 30px;
+  }
+
+  & form {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+  }
+
   & form label {
     display: flex;
     flex-direction: column;
+    font-weight: 600;
+  }
+  & form input {
+    padding: 10px 20px;
+    border: 1px solid var(--clr-dark-15);
+
+    &::placeholder {
+      font-weight: 400;
+    }
+  }
+
+  & form textarea {
+    padding: 10px 20px;
+    border: 1px solid var(--clr-dark-15);
+    resize: none;
+    height: 100px;
+
+    &::placeholder {
+      font-weight: 400;
+    }
   }
   & form div:first-of-type {
     display: flex;
     gap: 20px;
-  }
-  & form div:last-of-type {
-    margin-block: 30px;
   }
 `;
 
